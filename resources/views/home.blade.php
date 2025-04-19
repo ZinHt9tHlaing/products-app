18f64df2b8d487eb4802d45fbf01b84d27b4b655
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-04-19 11:33:52 +0630

    migration table and model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,14 @@
     <ul>
         @foreach ($products as $product)
             <li>
-                <a href="/products/{{ $product->id }}">
-                    {{ $product->name }}
-                </a>
-                @if ($product->name === 'Macbook')
-                    <span>( discount item )</span>
-                @endif
+                <h3>
+                    <a href="/products/{{ $product->id }}">
+                        {{ $product->name }}
+                    </a>
+                    @if ($product->is_discount)
+                        <span>( discount item )</span>
+                    @endif
+                </h3>
             </li>
         @endforeach
     </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Models\Product;
 use App\Models\TestProducts;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
     return view("home", [
         "title" => "Products",
-        "products" => TestProducts::all()
+        "products" => Product::all()
     ]);
 });
 
 Route::get(("/products/{id}"), function ($id) {
     return view("product-detail", [
         "title" => "Product Detail",
-        "product" => TestProducts::findOrFail(($id))
+        "product" => Product::findOrFail(($id))
     ]);
 });
